cebb9608990a8da2e0f536013155cceaeee96b50
Laura Sofia Dueñas Bulla <lauraduenas1105@gmail.com>
2021-12-30 23:58:58 -0500

    Generics services and DAOs

---
+++ src/main/java/backend/agricolas/utils/generics/GenericServiceImpl.java
@@ -0,0 +1,43 @@
+package backend.agricolas.utils.generics;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public abstract class GenericServiceImpl <T, ID extends Serializable> implements GenericService<T, ID> {
+
+    @Override
+    public T save(T entity) {
+        return getDao().save(entity);
+    }
+
+    @Override
+    public void delete(ID id) {
+        getDao().deleteById(id);
+    }
+
+    @Override
+    public T get(ID id) {
+        Optional<T> obj = getDao().findById(id);
+        if (obj.isPresent()) {
+            return obj.get();
+        }
+        return null;
+    }
+
+    @Override
+    public List<T> getAll() {
+        List<T> returnList = new ArrayList<>();
+        getDao().findAll().forEach(obj -> returnList.add(obj));
+        return returnList;
+    }
+
+    public abstract JpaRepository<T, ID> getDao();
+
+}
+
+
